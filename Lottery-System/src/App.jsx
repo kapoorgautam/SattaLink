507239a0b21c747e40b9b639e0e52cd6b2832a44
Ajay kumar <akjajay9315@gmail.com>
2024-09-05 19:37:07 +0530

    color chart

--- Lottery-System/src/App.jsx
+++ Lottery-System/src/App.jsx
@@ -1,51 +1,52 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios'
-import LandingPage from './components/LandingPage';
-import Admin from './components/Admin';
-import Errorpage from './components/Errorpage';
-import Signin from './components/Signin';
-import ColorChart from './components/ColorChart';
-import NumberChart from './components/NumberChart';
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+import LandingPage from "./components/LandingPage";
+import Admin from "./components/Admin";
+import Errorpage from "./components/Errorpage";
+import Signin from "./components/Signin";
+import ColorChart from "./components/ColorChart";
+import NumberChart from "./components/NumberChart";
 
 function App() {
-  const baseURL = "http://localhost:4000"
-  const userinfo = JSON.parse(localStorage.getItem('userinfo-Lucky'))
-  const [loginsuccessfully, setloginsuccessfully] = useState(false)
+  const baseURL = "http://localhost:4000";
+  const userinfo = JSON.parse(localStorage.getItem("userinfo-Lucky"));
+  const [loginsuccessfully, setloginsuccessfully] = useState(false);
   useEffect(() => {
-    axios.post(`${baseURL}/login`, userinfo)
-      .then(({ data }) => {
-        if (data?.isAdmin) setloginsuccessfully(true)
-      })
+    axios.post(`${baseURL}/login`, userinfo).then(({ data }) => {
+      if (data?.isAdmin) setloginsuccessfully(true);
+    });
   }, []);
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <LandingPage />
-    }, {
-      path: '/admin',
-      element: !loginsuccessfully ? <Signin /> : <Admin />
+      element: <LandingPage />,
     },
     {
-      path: '/color-chart',
-      element: <ColorChart />
-    }, {
-      path: '/number-chart',
-      element: <NumberChart />
+      path: "/admin",
+      element: !loginsuccessfully ? <Signin /> : <Admin />,
+    },
+    {
+      path: "/color-chart",
+      element: <ColorChart />,
+    },
+    {
+      path: "/number-chart",
+      element: <NumberChart />,
     },
     {
       path: "*",
-      element: <Errorpage />
-    }
-  ])
+      element: <Errorpage />,
+    },
+  ]);
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
